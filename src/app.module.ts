d8ad7d8f02cb9c61e877459cd303988acd141d21
ysys <1243933010@qq.com>
2024-06-26 15:51:11 +0800

    feat():npm cross-env  @nestjs/config 配置全局dev环境

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import {PrismaModule} from './prisma/prisma.module'
+import {PrismaModule} from './prisma/prisma.module';
+import { ConfigModule,ConfigService } from '@nestjs/config';  //@nestjs/config插件需要额外npm，使用它来给nest设定全局环境变量
+
+
+let envFilePath = ['.env'];   //判断指令执行的是dev还是pro
+if(process.env.NODE_ENV=='dev'){
+  envFilePath.push('.env.dev')
+}else{
+  envFilePath.push('.env.pro')
+}
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath
+    }),
     PrismaModule
   ],
   controllers: [AppController],
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {NestExpressApplication} from '@nestjs/platform-express'
+import {VersioningType} from '@nestjs/common'
+import { join } from 'path'
+import {Request,Response,NextFunction } from 'express'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule,{
+    logger:['error','warn','log']
+  });
+  // console.log(VersioningType.URI,process.env,'---')
+
+  app.enableVersioning({
+    type:VersioningType.URI
+  })
+
   await app.listen(3003);
 }
 bootstrap();
